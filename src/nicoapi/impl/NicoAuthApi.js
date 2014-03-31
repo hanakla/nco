94c17ecfd24ce10a38915c50c769741ade5a3142
0674272bac <0674272bac@anonymous>
2014-04-01 02:38:46 +0900

    NicoApiのフロント部分を作成

---
+++ src/nicoapi/impl/NicoAuthApi.js
@@ -0,0 +1,5 @@
+/*jslint node: true, vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, expr: true */
+define(function (require, exports, module) {
+    "use strict"; 
+    // TODO
+});
