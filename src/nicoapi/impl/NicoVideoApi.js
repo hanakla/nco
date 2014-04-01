984c013c563b052a746f9b6f2550ef93ad08b304
Ragg <Ragg-@users.noreply.github.com>
2014-04-02 05:50:22 +0900

    getVideoInfoメソッドが正しく動かなかった問題を修正

--- src/nicoapi/impl/NicoVideoApi.js
+++ src/nicoapi/impl/NicoVideoApi.js
@@ -12,18 +12,18 @@ define(function (require, exports, module) {
      * 動画情報(NicoVideoInfo）を取得します。
      * 
      * @param {string} movieId 情報を取得したい動画ID
-     * @return {$.Deferred} jQuery.Promiseオブジェクト。
+     * @return {$.Promise} jQuery.Promiseオブジェクト。
      * 動画情報が用意できれば取得したNicoVideoInfoオブジェクトと一緒にresolveされます。
      */
     function _getVideoInfo(movieId) {
         var deferred = $.Deferred(),
-            model = new NicoMovieInfo({id: movieId});
+            model = new NicoVideoInfo(movieId);
         
         model.fetch()
             .done(function () { deferred.resolve(model); })
             .fail(function (msg) { deferred.reject(msg); });
         
-        return deferred;
+        return deferred.promise();
     }
     
     exports._getVideoInfo = _getVideoInfo;
