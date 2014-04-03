1992fd913e653645aeca77a78bf50861dc92b98b
0674272bac <0674272bac@anonymous>
2014-04-03 21:08:32 +0900

    descapeHtmlメソッドを追加

--- src/utils/StringUtil.js
+++ src/utils/StringUtil.js
@@ -2,7 +2,11 @@
 define(function (require, exports, module) {
     "use strict";
     
-    var _ = require("thirdparty/lodash");
+    var htmlEscape = [
+        [/&lt;/g, "<"], [/&gt;/g, ">"],
+        [/&amp;/g, "&"], [/&quot;/g, "\""],
+        [/&#039;/g, "'"], [/&nbsp;/g, " "]
+    ];
     
     /**
      * str内の"%s"に第２引数以降の文字列を順番に埋め込みます。
@@ -10,19 +14,32 @@ define(function (require, exports, module) {
      *  String.format("Value: %s", obj.val);
      * 
      * @param {string} str 文字列
-     * @param {object...} obj 文字列に埋め込むパラメータ
+     * @param {Object...} obj 文字列に埋め込むパラメータ
      */
-    function _format(string) {
-        var args = _.toArray(arguments),
+    function _format(string, obj) {
+        var args = Array.prototype.slice.call(arguments),
             str = args.shift(),
             regexp = /%s/;
         
         while (regexp.test(str) && args.length) {
-            str = str.replace(/%s/, args.shift());
+            str = str.replace(regexp, args.shift());
         }
         
         return str;
     }
     
+    /**
+     * HTMLエスケープを解除します。
+     * @param {string} str エスケープを解く文字列
+     */
+    function _descapeHtml(str) {
+        htmlEscape.forEach(function (pair) {
+            str = str.replace(pair[0], pair[1]);
+        });
+        
+        return str;
+    }
+    
     exports.format = _format;
+    exports.descapeHtml = _descapeHtml;
 });
