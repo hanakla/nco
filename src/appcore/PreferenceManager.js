67148c912265890674c761c871ac807d07cdc5b4
Ragg <Ragg-@users.noreply.github.com>
2014-04-04 01:35:16 +0900

    PreferenceManager.jsを仮作成

---
+++ src/appcore/PreferenceManager.js
@@ -0,0 +1,22 @@
+/*jslint node: true, vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, expr: true */
+/*global define */
+
+/**
+ * TODO
+ */
+define(function (require, exports, module) {
+    "use strict";
+    
+    var _           = require("thirdparty/lodash"),
+        Backbone    = require("thirdparty/backbone");
+    
+    // TODO
+    function Preference() {}
+    
+    
+    function getModuleStorage(module) {
+        // TODO
+    }
+    
+    exports.getModuleStorage = getModuleStorage;
+});
