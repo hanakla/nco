61d621f7d482bc88d71b32573aa49ae0216e46ea
Ragg <Ragg-@users.noreply.github.com>
2014-04-06 03:07:50 +0900

    URLフィルタ拡張を実装(#35)

---
+++ src/extensions/url-linker/main.js
@@ -0,0 +1,19 @@
+/*jslint node: true, vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, expr: true */
+/*global nco, define*/
+define(function (require, exports, module) {
+    "use strict";
+    
+    var _           = nco.getModule("thirdparty/lodash"),
+        ContentsManager = nco.getModule("appcore/ContentsManager");
+    
+    var regexp = /(https?:\/\/[^\s　<>]+)/;
+    // http://d.hatena.ne.jp/sutara_lumpur/20100827/1282872312
+    
+    ContentsManager.addFilter("url-linker", function (tr, comment) {
+        // コメント内のURLを検索して置き換え
+        var $td = $(tr).find("td:first");
+        var html = $td.html();
+        console.log(html, html.match(regexp));
+        $td.html(html.replace(regexp, "<a href='$&'>$&</a>"));
+    });
+});
