e94f7ef461778a085c1942024ee7c4a7992b0c97
0674272bac <0674272bac@anonymous>
2014-04-06 23:49:45 +0900

    マイリストAPIフロントを整理しました。

--- src/nicoapi/impl/NicoMyListApi.js
+++ src/nicoapi/impl/NicoMyListApi.js
@@ -1,13 +1,22 @@
-/*jslint node: true, vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, expr: true */
+/*jslint node: true, vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, expr: true, eqnull: true */
+/*global $, define*/
 
 /**
  * ニコニコ動画のマイリスト操作APIのラッピングを行います。
  * （参考！: http://efcl.info/wiki/niconicoapi/）
  * 
  * Methods
- *  - getMyListGroups:$.Promise　-- マイリスト一覧を取得します。
- *      取得に成功したらMyListGroupの配列と一緒にresolveされ、
- *      失敗すればエラーメッセージとともにrejectされます。
+ *  - getMyListIndex(?withoutDefList:boolean):$.Promise
+ *      マイリストの簡略な一覧情報を取得します。
+ *      withoutDefListにtrueを指定すると"とりあえずマイリスト"を一覧から除外します。
+ *      取得に成功したら{Array.<OmittedMyListGroup>}をresolveし、
+ *      失敗した時はエラーメッセージとともにrejectされます。
+ * 
+ *  - getMyListGroup(?id:OmittedMyListGroup|number):$.Promise
+ *      指定されたOmittedMyListGroupまたはidと対応する、MyListGroupインスタンスを取得します。
+ *      取得できればMyListGroupオブジェクトと共にresolveされ、
+ *      そうでなければエラーメッセージと共にrejectされます
+ * 
  * Events
  * Properties
  */
@@ -18,18 +27,19 @@ define(function (require, exports, module) {
         Global      = require("utils/Global"),
         NicoAuthApi = require("./NicoAuthApi"),
         NicoUrl     = require("./NicoUrl"),
+        OmittedMyListGroup = require("../models/OmittedMyListGroup"),
         MyListGroup = require("../models/MyListGroup.js");
     
-    var FETCH_INTERVAL = 60000,
+    var 
+        // 一分以上経過したらトークンを取得する
+        FETCH_INTERVAL = 60000,
+        
+        // トークン抽出用パターン
         TOKEN_REGEXP = /NicoAPI.token = "([0-9a-z\-]*)";/;
     
     var _token = {
             timestamp: null,
             token: null,
-        },
-        _mylistGroups = {
-            timestamp: null,
-            groups: null,
         };
     
     /**
@@ -38,60 +48,50 @@ define(function (require, exports, module) {
      */
     function _fetchToken() {
         // 一定時間以内に取得したトークンがあればそれを返す
-        if (_token.token && _token.timestamp &&
-            (new Date() - _token.timestamp) < FETCH_INTERVAL
-        ) {
+        if (_token.token != null && (new Date() - _token.timestamp) < FETCH_INTERVAL) {
             return $.Deferred().resolve(_token.token).promise();
         }
         
-        // トークンがないか無効なら取得
+        //
+        // トークン取得
+        //
         var dfd = $.Deferred();
-        
         $.ajax({url: NicoUrl.MyList.FETCH_TOKEN, cache: false})
             .done(function (data) {
+                // データを取得したらトークンを取り出す
                 var token = TOKEN_REGEXP.exec(data);
                 
                 if (token && token[1]) {
                     token = token[1];
                     
+                    // キャッシュ
                     _token.timestamp = new Date();
                     _token.token = token;
                     
+                    // トークンを返す
                     dfd.resolve(token);
                 } else {
+                    // 何故か取り出せない
                     dfd.reject("トークンが取り出せませんでした。");
                 }
             })
             .fail(function (jqxhr, status, error) {
+                // 通信エラー
                 dfd.reject(error);
             });
         
         return dfd.promise();
     }
     
-    /**
-     * マイリスト一覧を取得します。
-     * @param {?boolean} nocache マイリスト一覧の最新の状態を取得するように指定します。
-     * @type {$.Promise} 取得に成功したら
-     */
-    function _getMyListGroups(nocache) {
-        var dfd = $.Deferred(),
-            getter = _myListGroupGetter.bind(null, dfd, nocache);
-        
-        NicoAuthApi.isLogin()
-            .done(getter)
-            .fail(function () {
-                NicoAuthApi.once("login", getter);
-            });
-        
-        return dfd.promise();
-    }
     
     /**
-     * 仮実装 TODO:そのうちコメント書いたりする。
-     * @param {Type} withoutDefList
+     * マイリストの簡略な一覧情報を取得します。
+     * @param {boolean} withoutDefList trueを指定すると"とりあえずマイリスト"を
+     *    一覧から除外します。
+     * @return {$.Promise} 取得に成功したら{Array.<OmittedMyListGroup>}をresolveします。
+     *    失敗した時はエラーメッセージとともにrejectされます。
      */
-    function __getMyListIndex(withoutDefList) {
+    function getMyListIndex(withoutDefList) {
         var dfd = $.Deferred(),
             lists = [];
         
@@ -104,86 +104,67 @@ define(function (require, exports, module) {
                 
                 // 受信したデータからインデックスを作成
                 _.each(res.mylistgroup, function (group) {
-                    lists.push({id:group.id, name:group.name})
+                    lists.push(new OmittedMyListGroup(group, getMyListGroup));
                 });
                 
                 // とりあえずマイリストを取得
                 if (withoutDefList !== true) {
-                    lists.push({id:"default", name:"とりあえずマイリスト"});
+                    lists.push(new OmittedMyListGroup(null, getMyListGroup));
                 }
                 
                 dfd.resolve(lists);
             })
             .fail(function (jqxhr, status, error) {
+                // 通信エラー
                 dfd.reject(error);
             });
         
         return dfd.promise();
     }
     
+    
     /**
-     * マイリスト一覧を実際に取得するリスナ関数
-     * @private
-     * @param {$.Deferred} dfd
-     * @param {boolean} nocache
+     * MyListGroupインスタンスを取得します。
+     * @param {OmittedMyListGroup|number} id OmittedMyListGroupかマイリストIDを渡します。
+     * @return {$.Promise} 取得できればMyListGroupオブジェクトと共にresolveされ、
+     *    そうでなければエラーメッセージと共にrejectされます
      */
-    function _myListGroupGetter(dfd, nocache) {
-        var isRequireFetch = (new Date() - _mylistGroups.timestamp) > FETCH_INTERVAL;
-        
-        // 更新すべきタイミングでない + キャッシュ非使用指定なし + データが一回はとってある
-        if (isRequireFetch === false && nocache !== true && _mylistGroups.groups !== null) {
-            // キャッシュを返す
-            dfd.resolve(_.clone(_mylistGroups.groups));
-            return;
+    function getMyListGroup(id) {
+        if (!id) {
+            id = "default";
         }
         
-        // APIからマイリスト一覧を取得
-        $.ajax({url:NicoUrl.MyList.GET_GROUPS, dataType:"json"})
+        var dfd = $.Deferred(),
+            getInstanceDfd = $.Deferred();
         
-            // レスポンス受信
-            .done(function (res) {
-                if (res.status !== "ok") {
-                    dfd.reject("不明なエラー(API接続完了)");
-                    return;
-                }
-                
-                // リストが初期化されていなければ初期化
-                _mylistGroups.groups = _mylistGroups.groups || {};
-                
-                // 最終更新日時を更新
-                _mylistGroups.timestamp = new Date();
-                
-                var cache = _mylistGroups.groups,
-                    groups = res.mylistgroup;
-                
-                // 受信したデータからMyListGroupインスタンスを生成
-                _.each(groups, function (group) {
-                    var old = cache[group.id];
+        if (id instanceof OmittedMyListGroup) {
+            getInstanceDfd.resolve(new MyListGroup(id));
+        } else {
+            
+            getMyListIndex().done(function (groups) {
+                _.each(groups, function (obj) {
                     
-                    if (old) {
-                        old.fetch();
-                    } else {
-                        cache[group.id] = new MyListGroup(group);
+                    // マイリストIDを元にインスタンスを取得
+                    if (obj.id === id) {
+                        getInstanceDfd.resolve(new MyListGroup(obj));
+                        return false;
                     }
                 });
                 
-                // とりあえずマイリストを取得
-                if (cache.default) {
-                    cache.default.fetch();
-                } else {
-                    cache.default = new MyListGroup();
-                }
-                
-                dfd.resolve(_.clone(cache));
-            })
-            .fail(function (jqxhr, status, error) {
-                dfd.reject(error);
+                getInstanceDfd.reject("指定されたIDのマイリストは見つかりませんでした。");
             });
-    }
-    
-    function __getMyListGroupFromId(id) {
-        var dfd = $.Deferred();
+        }
         
+        getInstanceDfd
+            .done(function (instance) {
+                instance.fetch()
+                    .done(function () { dfd.resolve(instance); })
+                    .fail(function (msg) { dfd.reject(msg); });
+            })
+            .fail(function (msg) { dfd.reject(msg); });
+        
+        
+        /*
         if (_mylistGroups.groups && _mylistGroups.groups[id]) {
             return dfd.resolve(_mylistGroups.groups[id]).promise();
         }
@@ -221,12 +202,11 @@ define(function (require, exports, module) {
             .fail(function (jqxhr, status, error) {
                 dfd.reject(error);
             });
-        
+        */
         return dfd.promise();
     }
     
     exports._fetchToken = _fetchToken;
-    exports.__getMyListGroupFromId = __getMyListGroupFromId;
-    exports.__getMyListIndex = __getMyListIndex;
-    exports.getMyListGroups = _getMyListGroups;
+    exports.getMyListIndex = getMyListIndex;
+    exports.getMyListGroup = getMyListGroup;
 });
