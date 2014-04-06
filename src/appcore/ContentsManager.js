c683bc1672ded97aff3c51bf923694c91d61f48d
Ragg <Ragg-@users.noreply.github.com>
2014-04-06 18:19:29 +0900

    コメントリストにpreタグを追加(#61)

--- src/appcore/ContentsManager.js
+++ src/appcore/ContentsManager.js
@@ -45,7 +45,7 @@ define(function (require, exports, module) {
             <tr data-userid="<%=user.id%>" data-premium="<%=user.isPremium%>"
                 data-date="<%=date.getTime()%>" data-score="<%=user.score%>"
                 data-command="<%=command%>" <%=isMyPost?"data-mypost":""%>>
-                <td><%=comment%></td>
+                <td><pre class="nco-comment-item"><%=comment%></pre></td>
             </tr>
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1]);
     
