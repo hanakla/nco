9a428a43b6a9ca934e6ccdda98f115c84ec6f802
Ragg <Ragg-@users.noreply.github.com>
2014-10-26 02:53:02 +0900

    Introduce stylus & require-stylus.

---
+++ src/config.require.js
@@ -0,0 +1,26 @@
+requirejs.config({
+    paths: {
+        cs: "vendor/require/cs",
+        text: "vendor/require/text",
+        jade: "vendor/require/jade",
+        styl: "vendor/require/styl",
+
+        "coffee-script": "vendor/coffee-script",
+        stylus: "vendor/stylus",
+        backbone: "vendor/backbone",
+        marionette: "vendor/backbone.marionette",
+        underscore: "vendor/underscore",
+        jquery: "vendor/jquery"
+    },
+    shim: {
+        backbone: {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+        marionette: {
+            deps: ["backbone"],
+            exports: "Backbone.Marionette"
+        }
+
+    }
+}, ["cs!entry"]);
